ff09f38a865dcd64785543f8fe1be17dab6b01a6
RafaDSan <rafaeldss12@gmail.com>
2025-08-12 19:30:35 -0300

    add initial websocket connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { middleware } from "#middlewares.js";
 import express from "express";
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("close", () => {
+    console.log("Client disconnedted");
+  });
+});
+
+console.log("Websocket Server stared on port 8080");
 
 const port = process.env.PORT ?? "9001";
 const app = express();
@@ -7,5 +20,5 @@ const app = express();
 app.get("/", middleware);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Express server started on port ${port}`);
 });
